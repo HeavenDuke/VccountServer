c7db66fb7365d4903753ccbd67709fb489e5e1be
HeavenDuke <trashlhc@163.com>
2017-09-03 15:18:30 +0800

    finish user and session module

--- controllers/accounts/index.js
+++ controllers/accounts/index.js
@@ -3,21 +3,31 @@
  */
 
 exports.index = async (ctx, next) => {
-
+    ctx.body = {
+        name: "account-index"
+    };
 };
 
 exports.show = async (ctx, next) => {
-
+    ctx.body = {
+        name: "account-show"
+    };
 };
 
 exports.create = async (ctx, next) => {
-
+    ctx.body = {
+        name: "account-create"
+    };
 };
 
 exports.update = async (ctx, next) => {
-
+    ctx.body = {
+        name: "account-update"
+    };
 };
 
 exports.destroy = async (ctx, next) => {
-
+    ctx.body = {
+        name: "account-destroy"
+    };
 };
